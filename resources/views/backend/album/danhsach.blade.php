0670ea902a88a28066710e27bf3a05176d960487
4un9cu0ng <3h08mi@gmail.com>
2018-01-16 13:34:10 +0700

    Album

--- resources/views/backend/album/danhsach.blade.php
+++ resources/views/backend/album/danhsach.blade.php
@@ -1,92 +1,13 @@
 @extends('backend/layout/base')
 @section('css')
     <style>
-        .hovereffect {
-            width:100%;
-            height:100%;
-            float:left;
-            overflow:hidden;
-            position:relative;
-            text-align:center;
-            cursor:default;
+        div#view-list-album .col-md-3 img {
+            width: 100%;
+            height: 140px;
         }
-
-        .hovereffect .overlay {
-            width:100%;
-            height:100%;
-            position:absolute;
-            overflow:hidden;
-            top:0;
-            left:0;
-            opacity:0;
-            background-color:rgba(44, 62, 80, 0.8);
-            -webkit-transition:all .4s ease-in-out;
-            transition:all .4s ease-in-out
-        }
-
-        .hovereffect img {
-            display:block;
-            position:relative;
-            -webkit-transition:all .4s linear;
-            transition:all .4s linear;
-        }
-
-        .hovereffect h2 {
-            text-transform:uppercase;
-            color:#fff;
-            text-align:center;
-            position:relative;
-            font-size:17px;
-            background:rgba(44, 62, 80, 0.8);
-            -webkit-transform:translatey(-100px);
-            -ms-transform:translatey(-100px);
-            transform:translatey(-100px);
-            -webkit-transition:all .2s ease-in-out;
-            transition:all .2s ease-in-out;
-            padding:10px;
-        }
-
-        .hovereffect a.info {
-            text-decoration:none;
-            display:inline-block;
-            text-transform:uppercase;
-            color:#fff;
-            border:1px solid #fff;
-            background-color:transparent;
-            opacity:0;
-            filter:alpha(opacity=0);
-            -webkit-transition:all .2s ease-in-out;
-            transition:all .2s ease-in-out;
-            margin:50px 0 0;
-            padding:7px 14px;
-        }
-
-        .hovereffect a.info:hover {
-            box-shadow:0 0 5px #fff;
-        }
-
-        .hovereffect:hover img {
-            -ms-transform:scale(1.2);
-            -webkit-transform:scale(1.2);
-            transform:scale(1.2);
-        }
-
-        .hovereffect:hover .overlay {
-            opacity:1;
-            filter:alpha(opacity=100);
-        }
-
-        .hovereffect:hover h2,.hovereffect:hover a.info {
-            opacity:1;
-            filter:alpha(opacity=100);
-            -ms-transform:translatey(0);
-            -webkit-transform:translatey(0);
-            transform:translatey(0);
-        }
-
-        .hovereffect:hover a.info {
-            -webkit-transition-delay:.2s;
-            transition-delay:.2s;
+        .col-md-3 {
+            margin-top: 25px;
+            font-size: 12px;
         }
     </style>
 @endsection
@@ -153,14 +74,15 @@
             })
         }
 
-        function drawHtml (obj) {
-            var str = '<div class="col-lg-3 col-md-4 col-sm-6 col-xs-12" style="padding: 5px ">\n' +
-                '<div class="hovereffect">\n' +
-                '<img style="height: 170px" class="img-responsive" src="upload/hinhanh/' + obj.hinhanh +'" alt="' + obj.hinhanh +'">\n' +
-                '<div class="overlay">\n' +
-                '<a class="info" href="javascript:void(0)" onclick="editAlbum(' + obj.id + ')"  style="color: #3498db" title="Sửa"><i class="fa fa-pencil fa-lg" aria-hidden="true"></i></a>\n' +
-                '<a class="info" href="javascript:void(0)" onclick="viewAlbum(' + obj.id + ')" style="color: #27ae60" title="Xem"><i class="fa fa-eye fa-lg" aria-hidden="true"></i></a>\n' +
-                '</div>\n' +
+        function drawHtml (data) {
+            var str = '<div class="col-md-3">\n' +
+                '<span class="mailbox-attachment-icon has-img"><img src="upload/hinhanh/'+ data.hinhanh +'" alt="'+ data.mota +'"></span>\n' +
+                '<div class="mailbox-attachment-info">\n' +
+                '<p><a href="upload/hinhanh/'+ data.hinhanh +'" class="mailbox-attachment-name">' + data.mota + '</a></p>\n' +
+                '<span>\n' +
+                '<button onclick="editAlbum(' + data.id +')" class="btn btn-primary" title="Sửa"><i class="fa fa-pencil" aria-hidden="true"></i></button>\n' +
+                '<button onclick="viewAlbum(' + data.id +')" class="btn btn-info" title="Xoá"><i class="fa fa-search-plus" aria-hidden="true"></i></button>\n' +
+                '</span>\n' +
                 '</div>\n' +
                 '</div>';
             return str;
