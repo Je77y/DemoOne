9adaab0bfd20a7aaf227a4ef868b276ee5ad4541
4un9cu0ng <3h08mi@gmail.com>
2018-01-19 00:25:41 +0700

    Chi tiết dự án

--- resources/views/frontEnd/duan/index.blade.php
+++ resources/views/frontEnd/duan/index.blade.php
@@ -6,10 +6,10 @@
     <div class="content-wrapper" >
         <div class="main-duan">
             <div class="center">
-                <img src="upload/devider.png">
+                <img class="diver" src="upload/devider.png">
                 <h2 class="nomargin duan-title">Dự án {{ $duan->tenchude }}</h2>
                 <div class="duan-hinhanh">
-                    <img src="upload/{{$duan->hinhanh}}">
+                    <img src="upload/hinhanh/{{$duan->hinhanh}}">
                 </div>
             </div>
             @foreach($duan->BlockContent as $key => $block)
@@ -22,19 +22,20 @@
                                 </div>
                             </div>
                         </div>
-                        @if($block->noidung == 'null')
-                            <div class="col-sm-12 nomargin nopadding body-img">
+                        @if($block->noidung == 'null' || $block->noidung == '<p>null</p>')
+                            <div class="col-sm-12 nomargin  body-img">
                                 @foreach($block->HinhAnh as $hinhanh)
                                     <img src="upload/hinhanh/{{ $hinhanh->url  }}" alt="{{ $block->tenblock }}">
                                 @endforeach
                             </div>
                         @else
-                            <div class="col-sm-5 nomargin nopadding ">
+                            <div class="col-sm-5 nomargin  ">
                                 <div class="body-text" >
                                     {!! $block->noidung !!}
                                 </div>
                             </div>
-                            <div class="col-sm-7 nomargin nopadding body-img">
+                            <div class="col-sm-7 nomargin  body-img">
+                                @if(count($block->HinhAnh) > 1)
                                 <div id="carousel-example-generic{{$key}}" class="carousel slide" data-ride="carousel">
                                     <ol class="carousel-indicators">
                                         @for($i = 0; $i < count($block->HinhAnh); $i++)
@@ -56,6 +57,9 @@
                                         <span class="fa fa-angle-right"></span>
                                     </a>
                                 </div>
+                                @elseif(count($block->HinhAnh) == 1)
+                                    <img src="upload/hinhanh/{{ $block->HinhAnh[0]->url  }}" alt="{{ $block->tenblock }}">
+                                @endif
                             </div>
                         @endif
                     </div>
@@ -68,14 +72,15 @@
                                 </div>
                             </div>
                         </div>
-                        @if($block->noidung == 'null')
-                            <div class="col-sm-12 nomargin nopadding body-img">
+                        @if($block->noidung == 'null'  || $block->noidung == '<p>null</p>')
+                            <div class="col-sm-12 nomargin  body-img">
                                 @foreach($block->HinhAnh as $hinhanh)
                                     <img src="upload/hinhanh/{{ $hinhanh->url  }}" alt="{{ $block->tenblock }}">
                                 @endforeach
                             </div>
                         @else
-                            <div class="col-sm-7 nomargin nopadding body-img">
+                            <div class="col-sm-7 nomargin  body-img">
+                                @if(count($block->HinhAnh) > 1)
                                 <div id="carousel-example-generic{{$key}}" class="carousel slide" data-ride="carousel">
                                     <ol class="carousel-indicators">
                                         @for($i = 0; $i < count($block->HinhAnh); $i++)
@@ -96,8 +101,11 @@
                                         <span class="fa fa-angle-right"></span>
                                     </a>
                                 </div>
+                                @elseif(count($block->HinhAnh) == 1)
+                                    <img src="upload/hinhanh/{{ $block->HinhAnh[0]->url  }}" alt="{{ $block->tenblock }}">
+                                @endif
                             </div>
-                            <div class="col-sm-5 nomargin nopadding ">
+                            <div class="col-sm-5 nomargin  ">
                                 <div class="body-text" >
                                     {!! $block->noidung !!}
                                 </div>
@@ -106,6 +114,47 @@
                     </div>
                 @endif
             @endforeach
+            <div class="blockcontent row nomargin">
+                <div class="blockcontent-header">
+                    <div class="row" style="margin-left: 0px; margin-right: 0px;">
+                        <div class="col-sm-12">
+                            <h1>Tài liệu dự án</h1>
+                        </div>
+                    </div>
+                </div>
+
+                @if(count($duan->TaiLieuDuAn) > 0)
+                <div class="col-sm-12 nomargin ">
+                    <div class="table-responsive">
+                        <table class="table">
+                            <thead>
+                            <tr>
+                                <th>STT</th>
+                                <th>Tên tài liệu</th>
+                                <th>Ngày cập nhật</th>
+                            </tr>
+                            </thead>
+                            <tbody>
+
+                                @foreach($duan->TaiLieuDuAn as $key => $tailieu)
+                                    <tr>
+                                        <td>{{ $key+1  }}</td>
+                                    <td>
+                                        <a href="upload/tailieu/{{ $tailieu->url }}">{{ $tailieu->tentailieu }}</a>
+                                    </td>
+                                    <td>
+                                        {{ $tailieu->updated_at }}
+                                    </td>
+                                    </tr>
+                                @endforeach
+
+                            </tbody>
+
+                        </table>
+                    </div>
+                </div>
+                @endif
+            </div>
         </div>
     </div>
 @endsection
